1d43195cee153967af806bd8cff348a0dea45c39
mbil-isd <mbil@isd.dp.ua>
2023-08-15 16:45:46 +0300

    fix exception on error;

--- src/main/java/ua/dp/isd/mbil/log4j/properties/converter/Main.java
+++ src/main/java/ua/dp/isd/mbil/log4j/properties/converter/Main.java
@@ -3,9 +3,9 @@ package ua.dp.isd.mbil.log4j.properties.converter;
 public class Main {
 
     public static void main(String[] args) {
-//        String path = "C:\\workspace\\esb-log4j2-patch";
+        String path = "C:\\workspace\\esb-log4j2-patch";
 //        boolean deleteAfterReading = true;
-        String path = ".";
+//        String path = ".";
         boolean deleteAfterReading = false;
         new ConfigConverter().run(path, deleteAfterReading);
     }
